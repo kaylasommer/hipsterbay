2647176cfdc4b599bdfa16d0b0d0fd063712d98b
Kayla Jones <KaylaLyne424@gmail.com>
2014-08-29 19:44:14 -0500

    commented out auction controller code

---
+++ app/controllers/auctions.js
@@ -0,0 +1,9 @@
+//exports.show = function(req, res){
+  //Auction.findById(req.params.auctionId, function(auction){
+    //if(res.locals.user._id === auction.ownerId){
+      //res.render('auctions/seller-show', {auction:auction});
+    //}else{
+      //res.render('auction/bidder-show', {auction:auction});
+    //}
+  //});
+//};
