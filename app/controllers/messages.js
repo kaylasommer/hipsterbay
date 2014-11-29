b0150f183f677976fdeaad20d1181dac1d655df0
Kayla Jones <KaylaLyne424@gmail.com>
2014-11-28 19:31:08 -0600

    mark as read on the inbox now works and greys out message

--- app/controllers/messages.js
+++ app/controllers/messages.js
@@ -4,7 +4,7 @@ var User = require('../models/user');
 
 exports.new = function(req, res){
   User.findById(req.params.receiverId, function(user){
-    res.render('messages/new-message', {receiver: user});
+    res.render('messages/reply', {receiver: user});
   });
 };
 
@@ -15,6 +15,3 @@ exports.send = function(req, res){
     });
   });
 };
-
-
-
